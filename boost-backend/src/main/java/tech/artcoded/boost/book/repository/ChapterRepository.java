2df4d7646c8815690b875230b0e95f97e4faac64
naterivah <contact@bittich.be>
2019-08-18 20:40:27 +0200

    order

--- boost-backend/src/main/java/tech/artcoded/boost/book/repository/ChapterRepository.java
+++ boost-backend/src/main/java/tech/artcoded/boost/book/repository/ChapterRepository.java
@@ -1,6 +1,7 @@
 package tech.artcoded.boost.book.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tech.artcoded.boost.book.entity.Book;
 import tech.artcoded.boost.book.entity.Chapter;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ChapterRepository extends JpaRepository<Chapter, Long> {
     List<Chapter> findByBook(Book book);
+
+    @Query("SELECT coalesce(max(chapter.order), 0) FROM Chapter chapter where chapter.book = 1")
+    int getMaxOrder(Book book);
 }
--- boost-backend/src/main/java/tech/artcoded/boost/book/service/ChapterService.java
+++ boost-backend/src/main/java/tech/artcoded/boost/book/service/ChapterService.java
@@ -28,12 +28,14 @@ public interface ChapterService extends CrudService<Long, Chapter> {
     @SneakyThrows
     default Chapter saveChapterAndUpload(ChapterDto chapter) {
         Upload upload = getUploadService().upload(chapter.getFile(), chapter.getContentType(), chapter.getFileName());
+        Optional<Book> optionalBook = getBookService().findById(chapter.getBookId());
+        Book book = optionalBook.orElseThrow(() -> new RuntimeException("cannot save chapter; book not found"));
         return getRepository().save(
                 Chapter.builder()
-                        .book(getBookService().getOne(chapter.getBookId()))
+                        .book(book)
                         .upload(upload)
                         .title(chapter.getTitle())
-                        .order(chapter.getOrder())
+                        .order(getRepository().getMaxOrder(book) + 1)
                         .description(chapter.getDescription())
                         .timeDuration(getUploadService().getAudioDuration(upload.getId()))
                         .build()
