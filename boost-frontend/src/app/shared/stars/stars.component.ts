860ddae70c83ff403bcd6e783bd7ee11ce959eef
Nordine Bittich <nordine@bittich.be>
2022-04-18 12:08:37 +0200

    cleanup

--- boost-frontend/src/app/shared/stars/stars.component.ts
+++ boost-frontend/src/app/shared/stars/stars.component.ts
@@ -1,56 +1,60 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Star } from '@core/models/star';
-import {faStar, faStarHalfAlt} from "@fortawesome/free-solid-svg-icons";
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
-  styleUrls: ['./stars.component.css']
+  styleUrls: ['./stars.component.css'],
 })
 export class StarsComponent implements OnInit {
-  faStar=faStar;
-  faStarHalfAlt= faStarHalfAlt;
+  faStar = faStar;
+  faStarHalfAlt = faStarHalfAlt;
 
   @Input()
-  public star:Array<Star>=[];
+  public star: Array<Star> = [];
 
   @Output()
-  public updateStar:EventEmitter<Star> = new EventEmitter();
+  public updateStar: EventEmitter<Star> = new EventEmitter();
 
-  public inputStar:Star=new Star();
+  public inputStar: Star = new Star();
 
   editMode: boolean;
 
   @Input()
-  public editable:boolean;
+  public editable: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   getIcon(i: number) {
     if (!this.star || !this.star.length) {
-      return ['fas','star'];
+      return ['fas', 'star'];
     }
 
-    let temp:number = this.star.map(s=> s.star).reduce((previousValue, currentValue) => {return previousValue + currentValue}) / this.star.length;
-    return this.getIconFromCurr(i,temp);
+    let temp: number =
+      this.star
+        .map((s) => s.star)
+        .reduce((previousValue, currentValue) => {
+          return previousValue + currentValue;
+        }) / this.star.length;
+    return this.getIconFromCurr(i, temp);
   }
 
-  getIconFromCurr(i:number, curr:number){
-    if (!curr){
-      return ['fas','star'];
+  getIconFromCurr(i: number, curr: number) {
+    if (!curr) {
+      return ['fas', 'star'];
     }
-    let st = Math.round(curr*2)/2;
-    if(st < i){
-      if (st +0.5 === i){
-        return ['fas','star-half-alt'];
-      }else {
-        return ['fas','star'];
+    let st = Math.round(curr * 2) / 2;
+    if (st < i) {
+      if (st + 0.5 === i) {
+        return ['fas', 'star-half-alt'];
+      } else {
+        return ['fas', 'star'];
       }
-    }else {
-      return ['fas','star'];
+    } else {
+      return ['fas', 'star'];
     }
   }
 
@@ -61,7 +65,7 @@ export class StarsComponent implements OnInit {
   }
 
   emitVote() {
-    if(!this.inputStar.star) {
+    if (!this.inputStar.star) {
       this.inputStar.star = 0;
     }
     this.updateStar.emit(this.inputStar);
